12f677006769c1eb8d9ede85d48db372fdc55ef3
Tarde <Tarde@ASATA405>
2021-07-16 18:16:01 +0200

    Version acabada

---
+++ Ejerciciobancoconsola/Ejerciciobancoconsola/Cuenta.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejerciciobancoconsola
+{
+    class Cuenta
+    {
+        private string numeroCuenta;
+        private string nombreCliente;
+        private float saldo=0;
+
+        public string NumeroCuenta{
+            get { return numeroCuenta;}
+            set {numeroCuenta=value;}
+        }   
+
+        public string NombreCliente
+        {
+            get { return nombreCliente; }
+            set { nombreCliente = value; }
+
+        }
+
+        public float Saldo
+        {
+            get { return saldo; }
+            set { saldo = value; }
+
+        }
+
+        public void depositar (float p1)
+        {
+            this.Saldo += p1;
+        }
+
+        public void retirar(float p1)
+        {
+            
+            if (saldo < p1)
+            {
+                Console.WriteLine("Error saldo no suficiente");
+                Console.ReadLine();
+                Console.Clear();
+
+            }
+            else
+            {
+                this.Saldo -= p1;
+
+            }
+                          
+        }
+
+        public void consultar()
+        {
+            Console.Clear();
+            Console.WriteLine("El saldo es :" + this.Saldo.ToString());
+            Console.ReadLine();
+            Console.Clear();
+
+        }
+
+
+    }
+}
---
+++ Ejerciciobancoconsola/Ejerciciobancoconsola/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejerciciobancoconsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+         {
+            string op = "";
+            Cuenta cta=new Cuenta();
+            float aux=0;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("1 Crear Cuenta ");
+                Console.WriteLine("2 Depositar ");
+                Console.WriteLine("3 Retirar ");
+                Console.WriteLine("4 Saldo ");
+                Console.WriteLine("5 Salir ");
+                op = Console.ReadLine();
+
+
+
+                switch (op)
+                {
+                    case ("1"):
+                        cta = new Cuenta();
+                        Console.WriteLine("Cuenta Creada");
+                        Console.ReadLine();
+                        break;
+
+                    case ("2"):
+                        Console.Clear();
+                        Console.WriteLine("Importe a ingresar");
+                        aux = float.Parse(Console.ReadLine());
+                        cta.depositar(aux);
+                        break;
+                    case ("3"):
+                        Console.Clear();
+                        Console.WriteLine("Importe a retirar");
+                        aux = float.Parse(Console.ReadLine());
+                        cta.retirar(aux);
+                        break;
+                    case ("4"):
+                        cta.consultar();
+                        break;
+
+                    default:
+                        Console.WriteLine("Saliendo");
+                        Console.ReadLine();
+                        break;
+
+                }
+            }
+            while (op != "5");
+
+
+
+
+
+            }
+    }
+}
