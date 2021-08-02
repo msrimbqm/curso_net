adea73904bd234f5746c9af66c06a7632bc1eae7
Tarde <Tarde@ASATA405>
2021-08-02 16:55:35 +0200

    con cuenta de buscados

--- LINQ2/SQLQ2Lambda/SQLQ2Lambda/Form1.Designer.cs
+++ LINQ2/SQLQ2Lambda/SQLQ2Lambda/Form1.Designer.cs
@@ -36,7 +36,7 @@ namespace SQLQ2Lambda
             this.label5 = new System.Windows.Forms.Label();
             this.txtid = new System.Windows.Forms.TextBox();
             this.txtnombre = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtape = new System.Windows.Forms.TextBox();
             this.txtedad = new System.Windows.Forms.TextBox();
             this.checkcasado = new System.Windows.Forms.CheckBox();
             this.btnalta = new System.Windows.Forms.Button();
@@ -45,13 +45,17 @@ namespace SQLQ2Lambda
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.btnbuscar = new System.Windows.Forms.Button();
             this.txtbuscar = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.labbus = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(62, 99);
+            this.label1.Location = new System.Drawing.Point(60, 129);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(18, 13);
             this.label1.TabIndex = 0;
@@ -60,7 +64,7 @@ namespace SQLQ2Lambda
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(62, 141);
+            this.label2.Location = new System.Drawing.Point(60, 171);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(44, 13);
             this.label2.TabIndex = 1;
@@ -69,7 +73,7 @@ namespace SQLQ2Lambda
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(62, 283);
+            this.label3.Location = new System.Drawing.Point(60, 313);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(43, 13);
             this.label3.TabIndex = 2;
@@ -78,7 +82,7 @@ namespace SQLQ2Lambda
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(62, 186);
+            this.label4.Location = new System.Drawing.Point(60, 216);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(44, 13);
             this.label4.TabIndex = 3;
@@ -87,7 +91,7 @@ namespace SQLQ2Lambda
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(63, 236);
+            this.label5.Location = new System.Drawing.Point(61, 266);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(32, 13);
             this.label5.TabIndex = 4;
@@ -95,28 +99,28 @@ namespace SQLQ2Lambda
             // 
             // txtid
             // 
-            this.txtid.Location = new System.Drawing.Point(158, 99);
+            this.txtid.Location = new System.Drawing.Point(156, 129);
             this.txtid.Name = "txtid";
             this.txtid.Size = new System.Drawing.Size(155, 20);
             this.txtid.TabIndex = 5;
             // 
             // txtnombre
             // 
-            this.txtnombre.Location = new System.Drawing.Point(158, 141);
+            this.txtnombre.Location = new System.Drawing.Point(156, 171);
             this.txtnombre.Name = "txtnombre";
             this.txtnombre.Size = new System.Drawing.Size(155, 20);
             this.txtnombre.TabIndex = 6;
             // 
-            // textBox1
+            // txtape
             // 
-            this.textBox1.Location = new System.Drawing.Point(158, 179);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(155, 20);
-            this.textBox1.TabIndex = 7;
+            this.txtape.Location = new System.Drawing.Point(156, 209);
+            this.txtape.Name = "txtape";
+            this.txtape.Size = new System.Drawing.Size(155, 20);
+            this.txtape.TabIndex = 7;
             // 
             // txtedad
             // 
-            this.txtedad.Location = new System.Drawing.Point(158, 229);
+            this.txtedad.Location = new System.Drawing.Point(156, 259);
             this.txtedad.Name = "txtedad";
             this.txtedad.Size = new System.Drawing.Size(155, 20);
             this.txtedad.TabIndex = 8;
@@ -124,7 +128,7 @@ namespace SQLQ2Lambda
             // checkcasado
             // 
             this.checkcasado.AutoSize = true;
-            this.checkcasado.Location = new System.Drawing.Point(158, 283);
+            this.checkcasado.Location = new System.Drawing.Point(156, 313);
             this.checkcasado.Name = "checkcasado";
             this.checkcasado.Size = new System.Drawing.Size(15, 14);
             this.checkcasado.TabIndex = 9;
@@ -132,7 +136,7 @@ namespace SQLQ2Lambda
             // 
             // btnalta
             // 
-            this.btnalta.Location = new System.Drawing.Point(75, 340);
+            this.btnalta.Location = new System.Drawing.Point(52, 365);
             this.btnalta.Name = "btnalta";
             this.btnalta.Size = new System.Drawing.Size(75, 23);
             this.btnalta.TabIndex = 10;
@@ -142,16 +146,17 @@ namespace SQLQ2Lambda
             // 
             // btnbaja
             // 
-            this.btnbaja.Location = new System.Drawing.Point(167, 340);
+            this.btnbaja.Location = new System.Drawing.Point(144, 365);
             this.btnbaja.Name = "btnbaja";
             this.btnbaja.Size = new System.Drawing.Size(75, 23);
             this.btnbaja.TabIndex = 11;
             this.btnbaja.Text = "baja";
             this.btnbaja.UseVisualStyleBackColor = true;
+            this.btnbaja.Click += new System.EventHandler(this.btnbaja_Click);
             // 
             // btnmodificar
             // 
-            this.btnmodificar.Location = new System.Drawing.Point(259, 340);
+            this.btnmodificar.Location = new System.Drawing.Point(236, 365);
             this.btnmodificar.Name = "btnmodificar";
             this.btnmodificar.Size = new System.Drawing.Size(75, 23);
             this.btnmodificar.TabIndex = 12;
@@ -164,24 +169,68 @@ namespace SQLQ2Lambda
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(359, 99);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(429, 264);
+            this.dataGridView1.Size = new System.Drawing.Size(429, 306);
             this.dataGridView1.TabIndex = 13;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
             // 
             // btnbuscar
             // 
-            this.btnbuscar.Location = new System.Drawing.Point(248, 38);
+            this.btnbuscar.Location = new System.Drawing.Point(266, 38);
             this.btnbuscar.Name = "btnbuscar";
             this.btnbuscar.Size = new System.Drawing.Size(75, 23);
             this.btnbuscar.TabIndex = 14;
             this.btnbuscar.Text = "Buscar";
             this.btnbuscar.UseVisualStyleBackColor = true;
+            this.btnbuscar.Click += new System.EventHandler(this.btnbuscar_Click);
             // 
             // txtbuscar
             // 
-            this.txtbuscar.Location = new System.Drawing.Point(49, 38);
+            this.txtbuscar.Location = new System.Drawing.Point(76, 38);
             this.txtbuscar.Name = "txtbuscar";
-            this.txtbuscar.Size = new System.Drawing.Size(155, 20);
+            this.txtbuscar.Size = new System.Drawing.Size(172, 20);
             this.txtbuscar.TabIndex = 15;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(26, 38);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Nombre";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(266, 67);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 17;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.AutoSize = true;
+            this.groupBox1.Location = new System.Drawing.Point(29, 99);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(312, 250);
+            this.groupBox1.TabIndex = 18;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // labbus
+            // 
+            this.labbus.AutoSize = true;
+            this.labbus.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labbus.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.labbus.Location = new System.Drawing.Point(29, 408);
+            this.labbus.Name = "labbus";
+            this.labbus.Size = new System.Drawing.Size(12, 18);
+            this.labbus.TabIndex = 19;
+            this.labbus.Text = " ";
             // 
             // txtapellido
             // 
@@ -189,6 +238,9 @@ namespace SQLQ2Lambda
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 433);
+            this.Controls.Add(this.labbus);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.txtbuscar);
             this.Controls.Add(this.btnbuscar);
             this.Controls.Add(this.dataGridView1);
@@ -197,7 +249,7 @@ namespace SQLQ2Lambda
             this.Controls.Add(this.btnalta);
             this.Controls.Add(this.checkcasado);
             this.Controls.Add(this.txtedad);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtape);
             this.Controls.Add(this.txtnombre);
             this.Controls.Add(this.txtid);
             this.Controls.Add(this.label5);
@@ -205,6 +257,7 @@ namespace SQLQ2Lambda
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
             this.Name = "txtapellido";
             this.Text = "sqlq lambda2 ";
             this.Load += new System.EventHandler(this.txtapellido_Load);
@@ -223,7 +276,7 @@ namespace SQLQ2Lambda
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox txtid;
         private System.Windows.Forms.TextBox txtnombre;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtape;
         private System.Windows.Forms.TextBox txtedad;
         private System.Windows.Forms.CheckBox checkcasado;
         private System.Windows.Forms.Button btnalta;
@@ -232,6 +285,10 @@ namespace SQLQ2Lambda
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button btnbuscar;
         private System.Windows.Forms.TextBox txtbuscar;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label labbus;
     }
 }
 
--- LINQ2/SQLQ2Lambda/SQLQ2Lambda/Form1.cs
+++ LINQ2/SQLQ2Lambda/SQLQ2Lambda/Form1.cs
@@ -25,13 +25,44 @@ namespace SQLQ2Lambda
             cargarGrid();
         }
 
+        void reset()
+        {
+
+            txtid.Text = "";
+            txtnombre.Text = "";
+            txtape.Text = "";
+            txtedad.Text = "";
+            checkcasado.Checked = false;
+            labbus.Text = "";
+        }
+
         void cargarGrid()
         {
-            var cargargrid = from p in bsa.Empleados select p;
-           
-            dataGridView1.DataSource = cargarGrid;
+            var x = from p in bsa.Empleados select p;
+
+            dataGridView1.DataSource = x;
 
         }
+
+        private void btnbuscar_Click(object sender, EventArgs e)
+        {
+            // busqueda por nombre y busco y muestro en grid
+           
+            
+            var x = from p in bsa.Empleados where txtbuscar.Text == p.Nombre select p;
+
+            
+            dataGridView1.DataSource = x;
+            
+            int cuenta = x.Count();
+            labbus.Text = "Se han encontrado "+cuenta.ToString()+" registros";
+
+
+
+
+
+        }
+
 
         private void btnalta_Click(object sender, EventArgs e)
         {
@@ -39,14 +70,27 @@ namespace SQLQ2Lambda
             Empleados MyEmpleado = new Empleados();
             MyEmpleado.Id = int.Parse(txtid.Text);
             MyEmpleado.Nombre = txtnombre.Text;
-            MyEmpleado.Apellido = txtapellido.Text;
+            MyEmpleado.Apellido = txtape.Text;
             MyEmpleado.Edad = int.Parse(txtedad.Text);
+            MyEmpleado.Casado = checkcasado.Checked;
 
-            if (checkcasado.Checked == true) MyEmpleado.Casado = 1 else MyEmpleado.Casado = 0
-
+           
+            try { 
             bsa.Empleados.InsertOnSubmit(MyEmpleado);
             bsa.SubmitChanges();
             cargarGrid();
+                reset();
+            
+
+
+
+
+                }
+        catch
+                {
+                MessageBox.Show("ERROR BASE DE DATOS");
+                
+                }
 
 
 
@@ -55,13 +99,75 @@ namespace SQLQ2Lambda
         private void btnmodificar_Click(object sender, EventArgs e)
         {
 
-            Empleados MyEmpleado = bsa.Empleados.Single(p => p.Id == int.Parse(txtid.Text);
+            Empleados MyEmpleado = bsa.Empleados.Single(p => p.Id == int.Parse(txtid.Text));
             MyEmpleado.Id = int.Parse(txtid.Text);
             MyEmpleado.Nombre = txtnombre.Text;
-            MyEmpleado.Apellido = txtapellido.Text;
+            MyEmpleado.Apellido = txtape.Text;
             MyEmpleado.Edad = int.Parse(txtedad.Text);
-            bsa.SubmitChanges();
+            MyEmpleado.Casado = checkcasado.Checked;
+            try
+            {
+
+                bsa.SubmitChanges();
+                cargarGrid();
+                reset();
+            }
+            catch
+            {
+                MessageBox.Show("ERROR BASE DE DATOS");
+            }
+
+        }
+
+        private void btnbaja_Click(object sender, EventArgs e)
+        {
+            Empleados MyEmpleado = bsa.Empleados.Single(x =>
+            x.Id == int.Parse(txtid.Text));
+
+            try
+            {
+                bsa.Empleados.DeleteOnSubmit(MyEmpleado);
+                bsa.SubmitChanges();
+                cargarGrid();
+                reset();
+            }
+
+            catch
+            {
+                MessageBox.Show("ERROR BASE DE DATOS");
+            }
+        }
+
+        
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
             cargarGrid();
+            labbus.Text = "";
+            
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+
+            
+
+           
 
         }
     }
