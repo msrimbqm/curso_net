cd1d33caab4a9b347ad917b6c56ca0fa5685194d
msrimbqm <msrimbqm@gmail.com>
2021-08-20 20:16:34 +0200

    casi terminado

---
+++ CodeFirst1/codefirst1/codefirst1/Migrations/202108201815087_Initial.cs
@@ -0,0 +1,29 @@
+namespace codefirst1.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.empleado",
+                c => new
+                    {
+                        IdEmpleado = c.Int(nullable: false, identity: true),
+                        Nombre = c.String(),
+                        Apellidos = c.String(),
+                        Direccion = c.String(),
+                        Email = c.String(),
+                    })
+                .PrimaryKey(t => t.IdEmpleado);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.empleado");
+        }
+    }
+}
---
+++ CodeFirst1/codefirst1/codefirst1/Models/ContextoEmpleados.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+
+namespace codefirst1.Models
+{
+    public class ContextoEmpleados:DbContext
+    {
+        public DbSet<empleado> Empleados { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+        }
+
+        public ContextoEmpleados() : base("CadenaEmpleados") { }
+        
+        
+
+    }
+}
