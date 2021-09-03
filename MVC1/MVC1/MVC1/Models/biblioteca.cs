7da27cf1be910c18446e40ca15ab777c5c13a436
msrimbqm <msrimbqm@gmail.com>
2021-09-03 20:28:46 +0200

    otro

---
+++ MVC1/MVC1/MVC1/Models/biblioteca.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC1.Models
+{
+    public class biblioteca
+    {
+        public List<libro> Libros { get; set; }
+        
+        public  biblioteca()
+        {
+            Libros = new List<libro>
+            {
+                new  libro  {  Isbn  =  "11122",  Titulo  =  "Los  Piratas  del  Caribe",  TipoLibro  =
+                        "Novela"},  new  libro  {  Isbn  =  "22211",  Titulo  =  "Los  Pilares  de  la  tierra",
+                        TipoLibro  =  "Novela"},  new  libro  {  Isbn  =  "33311",  Titulo  =  "Steve  Jobs",
+                        TipoLibro =  "Biografía"} 
+            };
+        }
+
+    }
+}
