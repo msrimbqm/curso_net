b3b5b85f2277bc912c60fd6c1dbc110096e00083
msrimbqm <msrimbqm@gmail.com>
2021-09-03 20:08:22 +0200

    mvc

--- MVC1/MVC1/MVC1/Controllers/DemoController.cs
+++ MVC1/MVC1/MVC1/Controllers/DemoController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVC1.Models;
 
 namespace MVC1.Controllers
 {
@@ -11,8 +12,22 @@ namespace MVC1.Controllers
         // GET: Demo
         public ActionResult Index()
         {
+            // esto  devuelve un libro solo
+            //var librox = new libro { Isbn = "1122", Titulo = "El Principito", TipoLibro = "Novela" };
+
+            // ahoa una list de libro
+            var librox = new List<libro>
+            {
+                new libro {Isbn = "1122", Titulo = "El Principito", TipoLibro = "Novela" },
+                new libro {Isbn = "1123", Titulo = "El Principito2", TipoLibro = "Novela romantica" }
+            };
+
+                     
+
+            
             ViewBag.Mensaje = "Acceso al metodo por defecto";
-            return View();
+            return View(librox);
         }
+
     }
 }
---
+++ MVC1/MVC1/MVC1/Models/libro.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC1.Models
+{
+    public class libro
+    {
+        public string Isbn { get; set; }
+        public string Titulo { get; set; }
+        public string TipoLibro { get; set; }                 
+
+    }
+
+
+
+}
